c8e00ad0b7a3b1240d8095d176e3c5d58cc8e0de
[Alejandro.DiGiovanni] <[jadigiova@gmail.com]>
2023-02-15 09:28:26 -0300

    Se crea el Header

--- app-maquetado-angular/src/app/app.module.ts
+++ app-maquetado-angular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadearComponent } from './components/headear/headear.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadearComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
